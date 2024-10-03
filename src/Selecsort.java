01eb21c1e79ae8c012e566100044edca4ab40235
preethamdnr <preethamdongari@gmail.com>
2024-10-03 12:26:59 -0500

    selection sort

--- src/Selecsort.java
+++ src/Selecsort.java
@@ -1,30 +1,25 @@
 public class Selecsort {
     public static void main(String[] args){
-        int nums[]={9,1,5,7,4,6};
-        int minIndex=-1;
-        int temp=0;
+        int arr[]={9,1,5,7,4,6};
         System.out.println("Elements before sorting: ");
-        for(int num:nums)
+        for(int num:arr)
         {
             System.out.print(num+" ");
         }
-        for(int i=0;i< nums.length-1;i++)
+        for(int i=1;i<arr.length;i++)
         {
-            minIndex=i;
-            for (int j=i+1;j< nums.length;j++)
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key)
             {
-                if(nums[minIndex]>nums[j])
-                {
-                    minIndex=j;
-                }
+                arr[j+1]=arr[j];
+                j--;
             }
-            temp=nums[minIndex];
-            nums[minIndex]=nums[i];
-            nums[i]=temp;
+            arr[j+1]=key;
         }
         System.out.println();
         System.out.println("Elements after sorting: ");
-        for(int num:nums)
+        for(int num:arr)
         {
             System.out.print(num+" ");
         }
