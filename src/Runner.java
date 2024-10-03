c4a08dee378d74840c386bbdcfc11410e6d9b88e
preethamdnr <preethamdongari@gmail.com>
2024-10-03 13:01:56 -0500

    linked list insertion

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+    public static void main(String[] args){
+        Linkedlist list=new Linkedlist();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
+        list.show();
+
+    }
+}
