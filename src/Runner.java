283058273b66cd959798d58f79470ad1f4875900
preethamdnr <preethamdongari@gmail.com>
2024-10-03 14:42:32 -0500

    linked list insertion & deletion

--- src/Runner.java
+++ src/Runner.java
@@ -6,6 +6,11 @@ public class Runner {
         list.insert(30);
         list.insert(40);
         list.insert(50);
+        list.insertAtStart(0);
+        list.insertAt(2,15);
+        list.insertAt(0,-1);
+        list.deleteAt(0);
+        list.deleteAt(5);
         list.show();
 
     }
