ba6db04073c389a156a4aefcf9b89668a3e1ab29
preethamdnr <preethamdongari@gmail.com>
2024-10-07 17:04:52 -0500

    Queue

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,23 @@
-public class Queue {
+public class Queue{
+    int queue[]= new int[5];
+    int front;
+    int rear;
+    int size;
+    public void enqueue(int data){
+        queue[rear]=data;
+        rear++;
+        size++;
+    }
+    public int dequeue(){
+        int data=queue[front];
+        front++;
+        size--;
+        return data;
+    }
+    public void show(){
+        System.out.print("Elements are: ");
+        for(int i=0;i<size;i++){
+            System.out.print(queue[front+i]+" ");
+        }
+    }
 }
--- src/RunnerQueue.java
+++ src/RunnerQueue.java
@@ -1,5 +1,12 @@
 public class RunnerQueue {
-    public static void main(){
-
+    public static void main(String[] args){
+        Queue obj= new Queue();
+        obj.enqueue(5);
+        obj.enqueue(6);
+        obj.enqueue(8);
+        obj.enqueue(9);
+        obj.dequeue();
+        obj.dequeue();
+        obj.show();
     }
 }
